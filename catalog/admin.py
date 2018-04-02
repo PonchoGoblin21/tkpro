05d6c22e969dae0674a40f94a7c13d42ac611689
Unknown <tarakeatinggg@gmail.com>
2018-04-02 13:34:02 +0100

    Stage 6. Home page finished.

---
+++ catalog/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+# Register your models here.
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'display_genre')
+	inlines = [BooksInstanceInline]
+admin.site.register(Book, BookAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth')
+admin.site.register(Author, AuthorAdmin)
+
+admin.site.register(Genre)
+
+class BookInstanceAdmin(admin.ModelAdmin):
+	list_filter = ('status', 'due_back')
+	fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+admin.site.register(BookInstance, BookInstanceAdmin)
